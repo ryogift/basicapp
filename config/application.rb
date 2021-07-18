820b6020fd8045fff0449cce7f80084a7f523993
Ryo Takahashi <ryogiftjp@gmail.com>
2021-07-18 13:43:45 +0900

    generateからassetsを除外

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module Basicapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.assets false
+    end
   end
 end
