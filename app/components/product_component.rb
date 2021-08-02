c200089a9a9bee458ed350b4f79b6e956d99e308
Ryo.gift <info.ry0takahash1@gmail.com>
2021-08-02 22:55:30 +0900

    ProductをViewComponentに書き換え

--- app/components/product_component.rb
+++ app/components/product_component.rb
@@ -1,5 +1,7 @@
 class ProductComponent < ViewComponent::Base
-  def initialize(product:)
-    @product = product
+  with_collection_parameter :item
+
+  def initialize(item:)
+    @item = item
   end
 end
