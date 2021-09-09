5890bffb48ef3e1ae67e476eb0022a4a359ae1be
Ryo Takahashi <ryogiftjp@gmail.com>
2021-09-09 23:14:59 +0900

    Layout/TrailingWhitespace

--- test/system/tops_test.rb
+++ test/system/tops_test.rb
@@ -3,7 +3,6 @@ require "application_system_test_case"
 class TopsTest < ApplicationSystemTestCase
   test "Topページに訪問する" do
     visit root_url
-  
     assert_selector "p", text: "BasicApp"
   end
 end
