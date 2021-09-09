225d31ac3345fbd2f22fe4e5b3eb552f6f0e4421
Ryo Takahashi <ryogiftjp@gmail.com>
2021-09-09 23:14:10 +0900

    Style/StringLiterals

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,7 +16,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create user" do
-    assert_difference('User.count') do
+    assert_difference("User.count") do
       post users_url, params: { user: { age: @user.age, name: @user.name } }
     end
 
@@ -39,7 +39,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy user" do
-    assert_difference('User.count', -1) do
+    assert_difference("User.count", -1) do
       delete user_url(@user)
     end
 
