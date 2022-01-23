b3e84388375589fc76d28306feff96bf1124b6d3
Ryo Takahashi <ryogiftjp@gmail.com>
2022-01-23 17:31:37 +0900

    Fix carrierwave for test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,3 +13,10 @@ CarrierWave.configure do |config|
     path_style: true
   }
 end
+
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+end
