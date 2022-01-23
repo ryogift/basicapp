e5f54e5925f421bad0fffc3cc125a257096e5c84
Ryo Takahashi <ryogiftjp@gmail.com>
2022-01-23 17:42:37 +0900

    Fix storage of carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,21 +2,19 @@ require "carrierwave/storage/abstract"
 require "carrierwave/storage/file"
 require "carrierwave/storage/fog"
 
-CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_directory = ENV["AWS_BUCKET"]
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: ENV["AWS_REGION"],
-    path_style: true
-  }
-end
+# CarrierWave.configure do |config|
+#   config.storage :fog
+#   config.fog_directory = ENV["AWS_BUCKET"]
+#   config.fog_credentials = {
+#     provider: "AWS",
+#     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+#     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+#     region: ENV["AWS_REGION"],
+#     path_style: true
+#   }
+# end
 
-if Rails.env.test?
-  CarrierWave.configure do |config|
-    config.storage = :file
-    config.enable_processing = false
-  end
+CarrierWave.configure do |config|
+  config.storage = :file
+  config.enable_processing = false
 end
